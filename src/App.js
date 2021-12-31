93b3210a8128e7e1a75b6b779002bc7138b66aa1
mkgwar <mohitgangwarknp@gmail.com>
2021-12-31 20:27:01 +0530

    boilerplate html code done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.scss";
-import logo from "./images/logo.svg";
-import cart from "./images/icon-cart.svg";
-import avatar from "./images/image-avatar.png";
+import {
+  logo,
+  cart,
+  avatar,
+  img1Thumb,
+  img1,
+  img2Thumb,
+  img2,
+  img3Thumb,
+  img3,
+  img4Thumb,
+  img4,
+  minus,
+  plus,
+} from "./ImageData";
+import { useState } from "react";
 
 function App() {
+  const [imageSRC, setimageSRC] = useState(img1);
+
   return (
     <div className="app">
       <nav className="nav">
@@ -22,10 +37,59 @@ function App() {
           <img src={avatar} alt="avatar" className="nav__avatar" />
         </div>
       </nav>
-      {/* Sneaker Company Fall Limited Edition Sneakers These low-profile sneakers
-      are your perfect casual wear companion. Featuring a durable rubber outer
-      sole, they’ll withstand everything the weather can offer. $125.00 50%
-      $250.00 0 Add to cart */}
+
+      <main className="main">
+        <div className="main__slider">
+          <img src={imageSRC} alt="slider" className="main__sliderMainiImage" />
+          <div className="main__sliderFlex">
+            <img
+              src={img1Thumb}
+              alt="slider"
+              onClick={() => setimageSRC(img1)}
+            />
+            <img
+              src={img2Thumb}
+              alt="slider"
+              onClick={() => setimageSRC(img2)}
+            />
+            <img
+              src={img3Thumb}
+              alt="slider"
+              onClick={() => setimageSRC(img3)}
+            />
+            <img
+              src={img4Thumb}
+              alt="slider"
+              onClick={() => setimageSRC(img4)}
+            />
+          </div>
+        </div>
+
+        <div className="main__text-content">
+          <div className="main__topText">Sneaker Company</div>
+          <h1 className="main__title">Fall Limited Edition Sneakers</h1>
+          <p className="main__paragraph">
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they’ll withstand everything
+            the weather can offer.
+          </p>
+          <div className="main__priceFlex">
+            <h2 className="main__price">$125.00</h2>
+            <div className="main__discount">50%</div>
+          </div>
+          <p className="main__originalPrice">$250.00</p>
+          <div className="main__addToCart">
+            <div className="main__plusMinus">
+              <img src={minus} alt="minus" /> 0
+              <img src={plus} alt="plus" />
+            </div>
+            <button className="main__addToCart">
+              <img src={cart} alt="cart" />
+              <span>Add To Cart</span>
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/ImageData.js
@@ -0,0 +1,29 @@
+import logo from "./images/logo.svg";
+import cart from "./images/icon-cart.svg";
+import avatar from "./images/image-avatar.png";
+import img1Thumb from "./images/image-product-1-thumbnail.jpg";
+import img1 from "./images/image-product-1.jpg";
+import img2Thumb from "./images/image-product-2-thumbnail.jpg";
+import img2 from "./images/image-product-2.jpg";
+import img3Thumb from "./images/image-product-3-thumbnail.jpg";
+import img3 from "./images/image-product-3.jpg";
+import img4Thumb from "./images/image-product-4-thumbnail.jpg";
+import img4 from "./images/image-product-4.jpg";
+import minus from "./images/icon-minus.svg";
+import plus from "./images/icon-plus.svg";
+
+export {
+  logo,
+  cart,
+  avatar,
+  img1Thumb,
+  img1,
+  img2Thumb,
+  img2,
+  img3Thumb,
+  img3,
+  img4Thumb,
+  img4,
+  minus,
+  plus,
+};
